ac5b92685d03341871732f6a0fcb5f277a65d3bc
vagrant <dsphere5296@gamil.com>
2021-03-07 10:31:01 +0000

    application_controller.rbの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,38 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_in_path_for(resource)
+		case resourece
+			when Admin
+				admin_admins_path
+			when Member
+				root_path
+			end
+		end
+	end 
+
+	def after_sign_up_path_for(resource)
+		case resource
+			when Admin
+				admin_admins_path
+			when Member
+				root_path
+		end
+	end
+
+	def after_sign_out_path_for(resource)
+		if resource == :admin
+			new_admin_session_path
+		else
+			new_member_session_path
+		end
+	end
+
+	protected
+
+	def configure_permitted_parameters
+		added_attrs = [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone_number, :email, :password, :password_confirmation]
+
+		devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+	end
 end
