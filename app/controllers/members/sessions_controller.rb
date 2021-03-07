df2f31267d7fb3fb934dc14b3cfcb313b60fdc8f
vagrant <dsphere5296@gamil.com>
2021-03-07 10:50:53 +0000

    members/sessions_controller.rbの記述

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -24,4 +24,22 @@ class Members::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  before_action :reject_member, only: [:create]
+
+  protected
+
+  def reject_member
+    @member = Member.find_by(email: params[:member][:email].downcase)
+    # "downcaseメソッド"は、全ての大文字を対応する小文字に置き換えた文字列を返すメソッド。
+    if @member
+      if (@member.valid_password?(params[:member][:password]) && (@member.active_for_authentication? == false))
+        # "valid_passwordメソッド"を利用することで、入力された現在のパスワードが正しいかチェックできる。
+        flash[:error] = "退会済みです。"
+        redirect_to new_member_session_path
+      end
+    else
+      flash[:error] = "必須項目を入力してください。"
+    end
+  end
 end
