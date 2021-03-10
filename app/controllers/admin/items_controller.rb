a19a7a78c8dfe212cf2e6f159a6e6437fa9ac4e5
vagrant <dsphere5296@gmail.com>
2021-03-10 18:48:43 +0000

    admin/items_controller.rbの記述

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,42 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_admin!
 
-  def edit
+  def index
+  	@items = Item.page(params[:page]).per(10)
   end
 
   def show
+  	@item = Item.find(params[:id])
   end
 
   def new
+  	@item = Item.new
   end
+
+  def create
+  	@item = Item.new(item_params)
+  	if @item.save
+  		redirect_to admin_items_path
+  	else
+  		render "new"
+  	end
+  end
+
+  def edit
+  	@item = Item.find(params[:id])
+  end
+
+  def update
+  	@item = Item.find(params[:id])
+  	if @item.update(item_params)
+  		redirect_to admin_items_path(@item.id)
+  	else
+  		render "edit"
+  	end
+  end
+
+  private
+
+  def item_params
+  	params.require(:item).permit(:image, :item_name, :explanation, :genre_id, :unit_price_without_tax, :sale_status)
 end
