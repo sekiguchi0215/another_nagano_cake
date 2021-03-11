ca4bc24cffab8e8093f8cc2390b59d038dc7297c
vagrant <dsphere5296@gmail.com>
2021-03-11 10:16:27 +0000

    管理者トップページの記述

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,4 +1,8 @@
 class Admin::AdminsController < ApplicationController
   def top
+  	range = Date.today.beginning_of_day..Date.today.end_of_day 
+  	# 本日の0時〜23時59分までのデータを指定。
+  	@order = Order.where(created_at: range)
+  	# "whereメソッド"で本日の注文データのみに限定する。
   end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,6 +11,7 @@ class Admin::ItemsController < ApplicationController
 
   def new
   	@item = Item.new
+  	@genres = Genre.all
   end
 
   def create
